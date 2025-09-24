37596783d04a6f7d2f54264a0e62f749cde20192
Роман <roma.zaxarov.05@gmail.com>
2025-09-24 19:36:34 +0500

    Second commit

---
+++ CppHomeworksProject/homework2.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std;
+int main() {
+    string name; //переменные
+    std::cout << "Print your name:";
+    std::cin >> name;//Ввод данных в консоль
+    std::cout << "Name: " << name;//вывод данных в консоль
+    return 0;
+}
