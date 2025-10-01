66ce5483cbdc5b13848358d052ff0f9769c160ed
Epanchin <88713179+Epanch1n@users.noreply.github.com>
2025-10-01 17:53:39 +0500

    Rename homework2.cpp to homework1.cpp

---
+++ CppHomeworksProject/homework1.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std;
+int main() {
+    string name; //ïåðåìåííûå
+    std::cout << "Print your name:";
+    std::cin >> name;//Ââîä äàííûõ â êîíñîëü
+    std::cout << "Name: " << name;//âûâîä äàííûõ â êîíñîëü
+    return 0;
+}
--- CppHomeworksProject/homework2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> 
-using namespace std;
-int main() {
-    string name; //переменные
-    std::cout << "Print your name:";
-    std::cin >> name;//Ввод данных в консоль
-    std::cout << "Name: " << name;//вывод данных в консоль
-    return 0;
-}
